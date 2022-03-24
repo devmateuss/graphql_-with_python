f50ade235a3f866d88cb3d12042c3936ba87edfa
Mateus Sousa <mateus.sousa@deway.com.br>
2022-03-24 17:54:35 -0300

    researches

---
+++ api/queries.py
@@ -0,0 +1,53 @@
+from .models import Post
+from ariadne import convert_kwargs_to_snake_case
+
+@convert_kwargs_to_snake_case
+def listPosts_resolver(obj, info):
+    try:
+        posts = [post.to_dict() for post in Post.query.all()]
+        payload = {
+            "success": True,
+            "posts": posts
+        }
+    except Exception as error:
+        payload = {
+            "success": False,
+            "errors": [str(error)]
+        }
+    print(payload)
+    return payload
+
+
+@convert_kwargs_to_snake_case
+def getPost_resolver(obj, info, id):
+    try:
+        post = Post.query.filter_by(id=id)
+        payload = {
+            "success": True,
+            "post": post.to_dict()
+        }
+    except AttributeError:
+        payload = {
+            "success": False,
+            "errors": ["Post item matching {id} not found"]
+        }
+    return payload
+
+
+@convert_kwargs_to_snake_case
+def getPost_resolver_by_title(obj, info, title):
+    print('to chamando')
+    try:
+        post = Post.query.filter_by(title=title).first()
+
+        print(post)
+        payload = {
+            "success": True,
+            "post": post.to_dict()
+        }
+    except AttributeError:
+        payload = {
+            "success": False,
+            "errors": ["Post item matching {id} not found"]
+        }
+    return payload
